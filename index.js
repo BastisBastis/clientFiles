fe66918e01c256ada2be92b742fa3f57700bf17f
Sebastian Gustafsson <sebgus@gmail.com>
2020-07-18 09:18:27 +0200

    index.js itemDB.js added

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+import Phaser from "phaser";
+import Game from "./scenes/game";
 
+const config = {
+		scale: {
+    	mode: Phaser.Scale.FIT,
+    	autoCenter: Phaser.Scale.CENTER_BOTH,
+  	},
+    type: Phaser.AUTO,
+    parent: "phaser-example",
+    width: 800,
+    height: 500,
+  	physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: [
+        Game
+    ]
+};
+
+const game = new Phaser.Game(config);
