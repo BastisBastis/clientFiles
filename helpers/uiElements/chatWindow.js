c6761381720716ee7f7bc5d7bb0210becd038d40
BastisBastis <BastisBastis>
2020-08-02 08:43:03 +0200

    changed chatwindow constructor y and width to tmpY etc

--- helpers/uiElements/chatWindow.js
+++ helpers/uiElements/chatWindow.js
@@ -4,16 +4,16 @@ export default class ChatWindow extends Phaser.GameObjects.Container {
     constructor ({
         scene,
         x=0,
-        y,
-        width,
+        tmpY,
+        tmpWidth,
         height=200,
         backgroundColor=UIConst.backgroundColor,
         alpha=UIConst.alpha,
         strokeColor=UIConst.strokeColor,
   	    strokeWidth=UIConst.strokeWidth
     }) {
-        y=y || scene.cameras.main.height-height;
-        width = width || scene.cameras.main.width-300;
+        y=tmpY || scene.cameras.main.height-height;
+        width = tmpWidth || scene.cameras.main.width-300;
         super(x,y)
         
         scene.add.existing(this);
