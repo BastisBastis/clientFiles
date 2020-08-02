1153df0ddb3049d5aa3c8b502b075bc955462592
BastisBastis <BastisBastis>
2020-08-02 08:47:07 +0200

    changed chatwindow to extend window

--- helpers/uiElements/chatWindow.js
+++ helpers/uiElements/chatWindow.js
@@ -1,31 +1,11 @@
 import * as UIConst from './uiConstants';
+import Window from './window';
 
-export default class ChatWindow extends Phaser.GameObjects.Container {
-    constructor ({
-        scene,
-        x=0,
-        tmpY,
-        tmpWidth,
-        height=200,
-        backgroundColor=UIConst.backgroundColor,
-        alpha=UIConst.alpha,
-        strokeColor=UIConst.strokeColor,
-  	    strokeWidth=UIConst.strokeWidth
-    }) {
-        y=tmpY || scene.cameras.main.height-height;
-        width = tmpWidth || scene.cameras.main.width-300;
-        super(x,y)
+export default class ChatWindow extends Window {
+    constructor ({config}) {
+       
+        super(config);
         
-        scene.add.existing(this);
-        
-        this.add(scene.add.rectangle(
-          x,
-          y,
-          width,
-          height, 
-          backgroundColor
-          ).setStrokeStyle(strokeWidth,strokeColor).setAlpha(alpha).setScrollFactor(0));
-          
         
         
         
