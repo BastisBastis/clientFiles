04cc9275179bdf9f15e4e0a0d73fc998534ded74
Bastis <sebgus@gmail.com>
2020-08-11 21:49:22 +0200

    removed .focus and kept .click on pointerup in messagebox

--- helpers/uiElements/chatWindow.js
+++ helpers/uiElements/chatWindow.js
@@ -66,7 +66,7 @@ export default class ChatWindow extends Window {
             if (!currTap.tap)
                 return;
                 
-            self.chatLine.getChildByName('chatLine').click().focus();
+            self.chatLine.getChildByName('chatLine').click();
             
         });
 
