d2cd92914b5458bd061b6bb6f98d0931570c9356
Bastis <sebgus@gmail.com>
2020-08-13 22:57:34 +0200

    asdf

--- helpers/uiElements/chatWindow.js
+++ helpers/uiElements/chatWindow.js
@@ -96,7 +96,7 @@ export default class ChatWindow extends Window {
                     setTimeout(() => {
 
                         // now we can focus on the target input
-                        self.chatLine.getChildByName('chatLine').click()
+                        self.chatLine.getChildByName('chatLine').click();
 
                         // cleanup
                         fakeInput.remove()
